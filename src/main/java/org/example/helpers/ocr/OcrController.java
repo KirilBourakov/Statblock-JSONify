351de142c7fe0c1e2503aeef79efa50117ad5497
Kiril Bourakov <kirbou06012@gmail.com>
2024-12-07 09:45:15 -0400

    fixed python scripts not being found

--- src/main/java/org/example/helpers/ocr/OcrController.java
+++ src/main/java/org/example/helpers/ocr/OcrController.java
@@ -10,20 +10,27 @@ import java.util.ArrayList;
 
 public class OcrController {
     public static ArrayList<String> read(String filepath){
-        String path = Paths.get(System.getProperty("user.dir"), "scripts/ocr/reader.py").toString();
+        String path = Paths.get(System.getProperty("user.dir"), "target/scripts/ocr/reader.py").toString();
         String pathToPython =  Paths.get(System.getProperty("user.dir"), "ocr_venv/Scripts/python.exe").toString();
         ProcessBuilder processBuilder = new ProcessBuilder(pathToPython, path, filepath);
+
+        System.out.println("BELOWWW");
         System.out.println(pathToPython);
+        System.out.println(path);
+        System.out.println(filepath);
         try {
             Process process = processBuilder.start();
             BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
             
             String line;
             ArrayList<String> lines = new ArrayList<>();
+            System.out.println("READING>......................");
             while ((line = reader.readLine()) != null) {
+                System.out.println(line);
                 lines.add(line);
             }
             int exitCode = process.waitFor();
+            System.out.println(exitCode);
             return lines;
             
         } catch (IOException | InterruptedException e) {
@@ -33,7 +40,7 @@ public class OcrController {
     }
 
     public static int setup(){
-        String path = Paths.get(System.getProperty("user.dir"), "scripts/ocr/setup.py").toString();
+        String path = Paths.get(System.getProperty("user.dir"), "target/scripts/ocr/setup.py").toString();
         ProcessBuilder processBuilder = new ProcessBuilder("python", path);
         System.out.println("path: " + path);
 
